f0b06a38e1f4a01fe424c3c688ed1cf146801a74
Kevin Finley <kftwotwo@gmail.com>
2017-01-27 09:16:59 -0800

    able to have dynamitc routing'

--- src/app/all-people/all-people.component.ts
+++ src/app/all-people/all-people.component.ts
@@ -1,25 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Member } from '../member.model';
 import { Router } from '@angular/router';
+import { MemberService } from '../member.service';
 
 @Component({
   selector: 'app-all-people',
   templateUrl: './all-people.component.html',
-  styleUrls: ['./all-people.component.css']
+  styleUrls: ['./all-people.component.css'],
+  providers: [MemberService]
 })
-export class AllPeopleComponent {
+export class AllPeopleComponent implements OnInit {
 
-  constructor(private router: Router){}
+  constructor(private router: Router, private memberService: MemberService){}
+  members: Member[];
 
-  members: Member[] = [
-     new Member("Kevin", "Finley",
-         "Manager", 'Fun to work with', 1),
-     new Member("Joe", "Bob",
-         "Cook", 'I have been cooking for 20 years', 2),
-     new Member("Nick", "Goldman",
-         "Head Chef", 'I love team work and I like keeping everyone together and being on the same page', 3)
-   ];
    goToDetailPage(clickedMember: Member) {
      this.router.navigate(['teams', clickedMember.id]);
    };
+
+   ngOnInit(){
+     this.members = this.memberService.getMembers(); 
+   }
   }
--- src/app/member-detail/member-detail.component.ts
+++ src/app/member-detail/member-detail.component.ts
@@ -2,16 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { Member } from '../member.model';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Location } from '@angular/common';
+import { MemberService } from '../member.service'
 
 @Component({
   selector: 'app-member-detail',
   templateUrl: './member-detail.component.html',
-  styleUrls: ['./member-detail.component.css']
+  styleUrls: ['./member-detail.component.css'],
+  providers: [MemberService]
 })
 export class MemberDetailComponent implements OnInit {
-
-  constructor() { }
+  memberId: number;
+  memberToDisplay: Member;
+  constructor(private route: ActivatedRoute, private location: Location, private memberService: MemberService) {}
 
   ngOnInit() {
+    this.route.params.forEach((urlParameters) => {
+      this.memberId = parseInt(urlParameters['id']);
+    });
+    this.memberToDisplay = this.memberService.getMemberById(this.memberId);
   }
 }
