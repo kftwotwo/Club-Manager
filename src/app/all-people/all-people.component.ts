b00ccec74dca877d3b737f2dbc92a7c5f2d1f0b7
Kevin Finley <kftwotwo@gmail.com>
2017-01-27 08:36:18 -0800

    able to view all people on route teams

--- src/app/all-people/all-people.component.ts
+++ src/app/all-people/all-people.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Member } from '../member.model'
 
 @Component({
   selector: 'app-all-people',
   templateUrl: './all-people.component.html',
   styleUrls: ['./all-people.component.css']
 })
-export class AllPeopleComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class AllPeopleComponent {
+  members: Member[] = [
+     new Member("Kevin", "Finley",
+         "Manager", 'Fun to work with', 1),
+     new Member("Joe", "Bob",
+         "Cook", 'I have been cooking for 20 years', 2),
+     new Member("Nick", "Goldman",
+         "Head Chef", 'I love team work and I like keeping everyone together and being on the same page', 3)
+   ];
 }
